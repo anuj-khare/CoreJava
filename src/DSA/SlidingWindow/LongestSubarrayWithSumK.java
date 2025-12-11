4242dbfb8d09f239abcc81dce06d4adf1d5989ed
Anuj khare <anujkhare92@gmail.com>
2025-12-12 00:28:40 +0530

    DSA Patterns Plan Added

---
+++ src/DSA/SlidingWindow/LongestSubarrayWithSumK.java
@@ -0,0 +1,12 @@
+package DSA.SlidingWindow;
+import java.util.*;
+
+public class LongestSubarrayWithSumK {
+    public static int LengthOfLongestSubarrayWithSumK(List<Integer> list,int k){
+        return -1;
+    }
+    public static void main(String[] args) {
+        List<Integer> A = Arrays.asList(10,5,2,7,1,-10);
+        A.forEach(System.out::println);
+    }
+}
